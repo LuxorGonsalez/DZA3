79ae56ddeb0d70f4c2319e54ca18dae4335ed716
LuxorGonsalez <luxor.gonsalez@gmail.com>
2022-10-24 17:17:16 +0300

    Tests updated

--- src/test/java/ru/netology/OrderPositiveTest.java
+++ src/test/java/ru/netology/OrderPositiveTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -41,7 +40,7 @@ public class OrderPositiveTest {
     public void shouldBeSuccessfulForm() {
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Евгений Перчаткин");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79870007700");
-        driver.findElement(By.cssSelector("[data-test-id=agreement] input")).click();
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         driver.findElement(By.cssSelector("button.button")).click();
         var actualText = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
